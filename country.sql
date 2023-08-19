e7fd08ffe4004c78173d2ab2634ae93a5fca9863
Yoshihiko Nakata <naka.sql.note@gmail.com>
2023-08-19 20:36:12 +0900

    Ver.2

--- country.sql
+++ country.sql
@@ -1,8 +1,7 @@
 SELECT DISTINCT
   geo.country AS country
 FROM
-  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`,
-  UNNEST([geo]) AS geo
+  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 WHERE
   _TABLE_SUFFIX BETWEEN '20201101' AND '20211031'
   AND geo.country IS NOT NULL
