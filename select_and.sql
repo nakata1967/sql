bbe39e5bfa8de21cd2c42f6ae75f92f3c95597d9
Yoshihiko Nakata <naka.sql.note@gmail.com>
2023-08-20 21:50:57 +0900

    Ver.2

--- select_and.sql
+++ select_and.sql
@@ -37,27 +37,3 @@ WHERE
   page_location, device_category, medium, source
 ORDER BY
   screen_page_views DESC
-
-
-
-
-
-
-
-
-
-
-
-
-SELECT DISTINCT
-  geo.country AS country,
-  device.category AS device_category,
-  traffic_source.medium AS medium,
-  traffic_source.source AS source,
-FROM
-  `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
-WHERE
-  _TABLE_SUFFIX BETWEEN '20201101' AND '20211031'
-  AND device.category = 'mobile'
-  AND traffic_source.medium = 'organic'
-  AND traffic_source.source = 'google'
