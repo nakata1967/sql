4f92438fa17adacb9ab37fc3978189d8b86e3399
Yoshihiko Nakata <naka.sql.note@gmail.com>
2023-08-22 21:11:03 +0900

    Ver.3

--- select.sql
+++ select.sql
@@ -1,15 +1,12 @@
 SELECT
   event_date AS date,
-  COUNT(
-      CASE
-      WHEN event_name = 'page_view' THEN 1
-      END
-  ) AS screen_page_views
+  COUNT(*) AS screen_page_views
 FROM
   `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 WHERE
   _TABLE_SUFFIX BETWEEN '20201101' AND '20201110'
+  AND event_name = 'page_view'
 GROUP BY
-  date
+  1
 ORDER BY
-  date
+  1;
